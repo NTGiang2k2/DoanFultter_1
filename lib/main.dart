a0513b464d6afa1e592a4d0b008f6c9d4fd06b7f
trieu.dev.da <trieu.dev.da@gmail.com>
2023-05-21 09:44:51 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+import 'Widget/Layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      title: 'Đồ án Flutter',
+      theme: ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home: LayoutWidget(title: 'Bán hàng'),
+      // builder: EasyLoading.init(),
+    );
+  }
+}
