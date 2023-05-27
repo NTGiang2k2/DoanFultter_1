aab033b843282f1a8bad494e6d878b1645a88ec6
NguyenDinhKhanhSon-CDTH18PMC <khanhson6999@gmail.com>
2023-05-27 10:49:56 +0700

    login

--- lib/View/Home/Home.dart
+++ lib/View/Home/Home.dart
@@ -1,3 +1,4 @@
+import 'package:do_an_flutter/View/Auth/Login/Login.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Text('Trang chủ');
+    return LoginPage();
   }
 }
--- lib/Widget/Layout.dart
+++ lib/Widget/Layout.dart
@@ -2,12 +2,9 @@ import 'package:do_an_flutter/View/Home/Home.dart';
 import 'package:do_an_flutter/View/Personal/Personal.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-<<<<<<< HEAD
 import '../View/Auth/Login/Login.dart';
 import '../View/Cart/GioHang.dart';
-=======
 import '../View/Product/Product.dart';
->>>>>>> 359d4d7b4fb3bcff4d9649908598ea86d94abb81
 import 'WidgetAll.dart';
 
 class LayoutWidget extends StatefulWidget {
@@ -21,15 +18,9 @@ class LayoutWidget extends StatefulWidget {
 class _LayoutWidgetState extends State<LayoutWidget> {
   TextEditingController _controllerSearch = TextEditingController();
   int _selectedIndex = 0;
-<<<<<<< HEAD
-  static List<Widget> _widgetOptions = <Widget>[
-    LoginPage(),
-    ProductPage(),
-=======
   static const List<Widget> _widgetOptions = <Widget>[
     HomePage(),
     lstItem(),
->>>>>>> 359d4d7b4fb3bcff4d9649908598ea86d94abb81
     PersonalPage(),
   ];
   void _onItemTapped(int index) {
@@ -44,7 +35,8 @@ class _LayoutWidgetState extends State<LayoutWidget> {
       appBar: AppBar(
         leading: const Center(child: FaIcon(FontAwesomeIcons.chevronLeft)),
         actions: [
-          IconButton(onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.gear)),
+          IconButton(
+              onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.gear)),
         ],
         title: Container(
           width: MediaQuery.of(context).size.width,
@@ -90,13 +82,8 @@ class Search extends StatelessWidget {
           color: Colors.white,
           onPressed: () {},
         ),
-<<<<<<< HEAD
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-=======
         border: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25.0)),
->>>>>>> 359d4d7b4fb3bcff4d9649908598ea86d94abb81
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
         ),
         hintStyle: const TextStyle(color: Colors.white),
         labelStyle: const TextStyle(color: Colors.white),
